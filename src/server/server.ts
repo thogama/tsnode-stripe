0197f11639095783edbd648acd67ac37fc2d6f5e
Alan Jonatas de Oliveira Gama <alan.gama@pmsc.local>
2023-03-09 10:19:34 -0300

    Client e Server separados

---
+++ src/server/server.ts
@@ -0,0 +1,32 @@
+import express from "express"
+import stripe from "./services/stripe"
+const app = express()
+const port = 3000
+
+
+app.post("/create-customer", async (req, res) => {
+    const customer = await stripe.customers.create({
+        description: 'Ditin User',
+    }).then((customer) => {
+        res.send((customer))
+    });
+})
+
+app.post("/create-payment-intent", async (req, res) => {
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            currency: "brl",
+            amount: 2000,
+            automatic_payment_methods: {
+                enabled: true,
+            }
+        })
+        res.send({ clientSecret: paymentIntent.client_secret })
+    } catch (e) {
+        return res.status(400).send({ e })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Backend http://localhost:${port}`)
+})
